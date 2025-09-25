2d017d34df6948bb098ba0fd2cd1d6a53303a456
Kalpa Sharma <kobonew23@gmail.com>
2025-09-25 18:11:04 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+Addition=a+b
+Subtraction=a-b
+Multiplication=a*b
+Division=a/b
+print('Addition', Addition)
+print('Subtraction', Subtraction)
+print('Multiplication', Multiplication)
+print('Division', Division)
---
+++ Task 2.py
@@ -0,0 +1,9 @@
+
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+c=a + " " + b
+print (c)
+print("Hello, "+c+"! Welcome to the Phyton program")
+
+
+
